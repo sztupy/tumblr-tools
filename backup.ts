e8399b5a1a8109efa24b36313139bf4b2b16f7a6
Zsolt Raven <zsolt.raven@b4bpayments.com>
2025-05-07 13:12:53 +0100

    Add support for backing up media

--- backup.ts
+++ backup.ts
@@ -6,13 +6,14 @@ import { eachLimit } from 'async';
 import archiver from 'archiver';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { JSDOM } from "jsdom";
+import Axios from 'axios';
+import * as stream from 'stream';
+import { promisify } from 'util';
+import YAML from 'yaml'
 
 const client = createClient(environment.tumblrKeys);
-const usernames = fs.readFileSync('blogs.dat').toString().trim().split("\n");
-
-const MAX_DASHBOARD_OFFSET = 100;
-const CONCURRENCY = 10;
-const DUMP_MEDIA = false;
+const config = YAML.parse(fs.readFileSync(process.argv[2] || 'backup_config.yaml').toString())
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename);
@@ -21,8 +22,91 @@ if (!fs.existsSync(__dirname + "/dump")) {
   fs.mkdirSync(__dirname + "/dump");
 }
 
+const finished = promisify(stream.finished);
+const recentFiles: Record<string, boolean> = {}
+
+function getConfig(username: string, key: string, defaultValue: any = null) {
+  return config.users[username][key] || config.defaults[key] || defaultValue
+}
+
+async function downloadFile(fileUrl: string, outputLocationPath: string): Promise<any> {
+  const cacheKey = fileUrl + ':' + outputLocationPath
+  if (!recentFiles[cacheKey]) {
+    recentFiles[cacheKey] = true;
+    const writer = fs.createWriteStream(outputLocationPath);
+    return Axios({
+      method: 'get',
+      url: fileUrl,
+      responseType: 'stream',
+    }).then(response => {
+      response.data.pipe(writer);
+      return finished(writer);
+    });
+  }
+}
+
+async function downloadImage(url: string, blogName: string, postBlogName: string) {
+  if (getConfig(blogName, 'dump_media', false) && (blogName == postBlogName || getConfig(blogName, 'dump_all_media', false))) {
+    let name = "";
+    let path = "";
+    if (url.match("tumblr.com")) {
+      const fragments = url.split('/')
+      while (name.length < 8)
+        name = name + fragments.pop()
+
+      path = `${__dirname}/dump/tumblr_files/${name}`
+    } else {
+      name = url.replaceAll(/[^a-zA-Z0-9._-]/, '_')
+      path = `${__dirname}/dump/tumblr_files/external/${name}`
+    }
+
+    if (!fs.existsSync(__dirname + "/dump/tumblr_files")) {
+      fs.mkdirSync(__dirname + "/dump/tumblr_files");
+    }
+    if (!fs.existsSync(__dirname + "/dump/tumblr_files/external")) {
+      fs.mkdirSync(__dirname + "/dump/tumblr_files/external");
+    }
+
+    if (!fs.existsSync(path) || !fs.statSync(path).size) {
+      await downloadFile(url, path);
+    }
+  }
+}
+
+async function fetchPhotos(post: any, blogName: string) {
+  const firstTrail = post.trail.first
+  const trailUser = firstTrail?.blog.name
+
+  for (const element of post.photos) {
+    await fetchPhoto(element, blogName, post.reblogged_root_name || trailUser || post.blog_name);
+  };
+}
+
+async function fetchPhoto(photo: any, blogName: string, postBlogName: string) {
+  const sizes = photo.alt_sizes
+  if (photo.original_size)
+    sizes.push(photo.original_size)
+
+  if (!sizes)
+    return
+
+  sizes.sort((a: any, b: any) => b.width - a.width);
+
+  for (const size of sizes) {
+    const url = size.url;
+    if (!url)
+      continue;
+
+    try {
+      await downloadImage(url, blogName, postBlogName);
+      return;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) { /* empty */ }
+  }
+}
+
 try {
-  await eachLimit(usernames, CONCURRENCY, async function (username) {
+  await eachLimit(Object.keys(config.users), config.concurrency || 10, async function (username) {
     try {
       if (!fs.existsSync(__dirname + "/dump/" + username)) {
         fs.mkdirSync(__dirname + "/dump/" + username);
@@ -30,11 +114,13 @@ try {
       console.log("START %s", username);
       const options = {
         limit: 50,
-        offset: 0,
-        reblog_info: true
+        offset: getConfig(username, 'offset', 0),
+        reblog_info: true,
+        type: 'audio'
       }
       while (true) {
         const [body,] = username == 'dashboard' ? await client.userDashboardAsync(options) : await client.blogPostsAsync(username, options);
+        const promises: Promise<any>[] = [];
 
         if (options.offset === 0) {
           console.log("BLOG %s", username);
@@ -46,10 +132,74 @@ try {
           console.log("PRG %s %s", username, ((options.offset / body.blog.total_posts) * 100).toFixed(3) + "%");
         }
 
+        if (getConfig(username, 'dump_media', false)) {
+          for (const post of body.posts) {
+            const first_trail = post.trail[0]
+            const trail_user = first_trail?.blog.name
+            const poster_user = post['reblogged_root_name'] || trail_user || post['blog_name']
+
+            if (post.type == "video") {
+              try {
+                const player = post.player.sort((a: any, b: any) => b.width - a.width)[0].embed_code
+
+                const dom = new JSDOM(player);
+                for (const el of dom.window.document.getElementsByTagName('source')) {
+                  const source = el.getAttribute('src')
+                  if (source) {
+                    promises.push(downloadImage(source, username, poster_user));
+                  }
+                }
+
+                for (const el of dom.window.document.getElementsByTagName('video')) {
+                  const source = el.getAttribute('poster')
+                  if (source) {
+                    promises.push(downloadImage(source, username, poster_user));
+                  }
+                }
+              } catch (error) {
+                console.log("Could not download video");
+                console.log(error);
+              }
+            } else if (post.type == "audio" && post.audio_url && post.audio_url.match("tumblr.com")) {
+              promises.push(downloadImage(post.audio_url, username, poster_user));
+            } else if (post.type == "photo") {
+              if (post.photos)
+                promises.push(fetchPhotos(post, username));
+            } else if (post.type == "link") {
+              if (post.photos)
+                promises.push(fetchPhotos(post, username));
+            }
+
+            for (const trail of post.trail) {
+              const content = trail.content_raw.replaceAll('[[MORE]]', '<!-- [[MORE]] -->')
+              try {
+                const dom = new JSDOM(content);
+                for (const el of dom.window.document.getElementsByTagName('img')) {
+                  const source = el.getAttribute('src')
+                  if (source) {
+                    promises.push(downloadImage(source, username, post.blog.name));
+                  }
+                }
+              } catch (error) {
+                console.log("Could not download image");
+                console.log(error);
+              }
+            }
+          }
+        }
+
+        try {
+          await Promise.all(promises);
+        } catch (error) {
+          console.log(error);
+        }
+
         const json = JSON.stringify(body);
         fs.writeFile('dump/' + username + '/dump-' + options.offset + '.json', json, 'utf8', function () { });
 
-        if (username == 'dashboard' ? MAX_DASHBOARD_OFFSET > options.offset : body.posts.length >= options.limit) {
+        const max_offset = getConfig(username, 'max_offset', null)
+
+        if (body.posts.length >= options.limit && (!max_offset || options.offset < max_offset)) {
           options.offset += options.limit;
         } else {
           console.log("DONE %s", username);
--- lib/tumblr/index.ts
+++ lib/tumblr/index.ts
@@ -2,8 +2,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createClient as tumblrCreateClient, Client } from "tumblr.js";
 
-function findRateLimitHeader(headers: any, name : string) {
-  return Object.keys(headers).filter(header => header.match('ratelimit') && header.match(name)).map(header => headers[header])[0];
+function findRateLimitHeaders(headers: any) {
+  const result: Record<string, Record<string, number>> = {}
+
+  for (const headerName in headers) {
+    if (headerName.toLowerCase().match('ratelimit')) {
+      const split = headerName.replace('_', '-').split('-');
+      const value = +headers[headerName];
+      const valueType = split[split.length - 1];
+      const rateType = split[split.length - 2];
+      result[rateType] ||= {};
+      result[rateType][valueType] = value;
+    }
+  }
+
+  return result
 }
 
 function tumblrPromisify(func : Function) : () => Promise<any> {
@@ -25,21 +38,32 @@ function tumblrPromisify(func : Function) : () => Promise<any> {
             }
           } else if (dataDetailed.statusCode == 429) {
             // it's a rate limit. let's retry once the limit is gone
-            const limit = findRateLimitHeader(dataDetailed.headers, '-limit');
-            const remaining = findRateLimitHeader(dataDetailed.headers, '-remaining');
-            const reset = findRateLimitHeader(dataDetailed.headers, '-reset');
+            const headers = findRateLimitHeaders(dataDetailed.headers);
+
+            let type = 'unknown';
+            let limit = 0;
+            let remaining = 99999999;
+            let reset = 10;
+
+            for (const headerType in headers) {
+              if (remaining > headers[headerType]['remaining']) {
+                type = headerType;
+                limit = headers[headerType]['limit'];
+                remaining = headers[headerType]['remaining'];
+                reset = headers[headerType]['reset'];
+              }
+            }
 
             let timeout = 10000;
             if (remaining == 0) {
               timeout = reset;
             }
 
-            if (retries > 0){
-              console.log(`API limit exceeded: L:${limit} / R:${remaining} / S:${reset}; waiting ${timeout}ms; Retries left ${retries}`);
-              setTimeout(() => func.apply(self, [...args, rateLimiterFunction]), timeout);
-            } else {
-              reject([err, dataDetailed]);
-            }
+            retries = 4;
+            console.log("API limit exceeded");
+            console.log(headers);
+            console.log(`T:${type} / L:${limit} / R:${remaining} / W:${reset}; waiting ${timeout}s`);
+            setTimeout(() => func.apply(self, [...args, rateLimiterFunction]), timeout * 1000);
           } else {
              // 404s and other issues
             if (retries>0) {
